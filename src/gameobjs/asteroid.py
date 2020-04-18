584cc3011e6a413a8afb6b1a1a1d903a4ea1cff2
Payne325 <alex.payne325@gmial.com>
2020-04-18 13:46:47 +0100

    Added asteroid class

---
+++ src/gameobjs/asteroid.py
@@ -0,0 +1,25 @@
+from src.InjanStructures import *
+from src.InjanKeycodes import *
+
+class Asteroid:
+   def __init__(self, engine, pos, weight):
+      self.engine = engine
+      self.pos = pos
+      self.weight = weight
+      self.floor_height = 0
+
+      #todo swap texture for asteroid
+      spriteID = engine.CreateTexture("assets/sprites/player.png", Vector2(0, 0), Vector2(512, 512))
+      self.sprite = engine.CreateSprite(Vector3(0, 0, 1.0), Vector2(32 ,32), spriteID)
+
+   def update_floor_height(self, height):
+      self.floor_height = height
+
+   def update_position(self, dt):
+      vel = Vector3(0.0, self.weight * dt, 0.0)
+      self.pos += vel
+
+      if self.pos.y < self.floor_height:
+         self.pos.y = self.floor_height
+         
+      self.sprite.SetPosition(Vector3(self.pos.x * 32, self.pos.y * 32, 1.0))
